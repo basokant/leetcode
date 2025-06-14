852bfc639cf28ede0247970dd5cde90c6ac16fef
basokant <basokanthan@gmail.com>
2025-06-14 19:23:55 -0400

    finish valid palindrome

---
+++ 125.valid-palindrome.py
@@ -0,0 +1,18 @@
+# @leet start
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        alpha_s = "".join(c for c in list(s) if c.isalnum()).lower()
+        left, right = 0, len(alpha_s) - 1
+
+        while left < right:
+            if alpha_s[left] != alpha_s[right]:
+                return False
+
+            left += 1
+            right -= 1
+
+        return True
+
+
+# @leet end
+
