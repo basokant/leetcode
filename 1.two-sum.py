6f64042843ec97525d94b6fa9efc28d93d2dd5b3
basokant <basokanthan@gmail.com>
2025-06-13 20:40:31 -0400

    finish two-sum

---
+++ 1.two-sum.py
@@ -0,0 +1,15 @@
+# @leet start
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        numToIdxMap = dict((num, idx) for idx, num in enumerate(nums))
+
+        for idx, num in enumerate(nums):
+            otherNum = target - num
+            otherIdx = numToIdxMap[otherNum] if otherNum in numToIdxMap else -1
+            if otherIdx > 0 and otherIdx != idx:
+                return [idx, otherIdx]
+
+        return [-1, -1]
+
+
+# @leet end
