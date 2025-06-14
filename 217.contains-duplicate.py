7a0a232133e2ba5c551ec98ab0f9db0414cd9c07
basokant <basokanthan@gmail.com>
2025-06-14 19:23:40 -0400

    finish contains duplicate

---
+++ 217.contains-duplicate.py
@@ -0,0 +1,16 @@
+# @leet start
+class Solution:
+    def containsDuplicate(self, nums: list[int]) -> bool:
+        numsSet = set()
+
+        for num in nums:
+            if num in numsSet:
+                return True
+
+            numsSet.add(num)
+
+        return False
+
+
+# @leet end
+
