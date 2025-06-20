8637ec796e537b756ba47569f5c57e418c3d4b48
basokant <basokanthan@gmail.com>
2025-06-19 23:23:14 -0400

    finish 3sum

---
+++ 15.3sum.py
@@ -0,0 +1,34 @@
+# @leet start
+class Solution:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        res = []
+        sorted_nums = sorted(nums)
+
+        for i, num1 in enumerate(sorted_nums):
+            if num1 > 0:
+                break
+
+            if i > 0 and num1 == sorted_nums[i - 1]:
+                continue
+
+            j, k = i + 1, len(sorted_nums) - 1
+
+            while j < k:
+                num2, num3 = sorted_nums[j], sorted_nums[k]
+                threeSum = num1 + num2 + num3
+
+                if threeSum > 0:
+                    k -= 1
+                elif threeSum < 0:
+                    j += 1
+                else:
+                    res.append([num1, num2, num3])
+                    j += 1
+                    k -= 1
+                    while j < k and sorted_nums[j] == sorted_nums[j - 1]:
+                        j += 1
+
+        return res
+
+
+# @leet end
