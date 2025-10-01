c877ef807e4e7000a2e61c0eb5698b1ca8df5edb
basokant <basokanthan@gmail.com>
2025-10-01 15:16:50 -0400

    finish subtree of another tree

---
+++ 572.subtree-of-another-tree.py
@@ -0,0 +1,35 @@
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q:
+            return True
+        elif not p or not q or p.val != q.val:
+            return False
+
+        left = self.isSameTree(p.left, q.left)
+        right = self.isSameTree(p.right, q.right)
+
+        return left and right
+
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        if not root and not subRoot:
+            return True
+        elif not root or not subRoot:
+            return False
+        elif root.val == subRoot.val and self.isSameTree(root, subRoot):
+            return True
+
+        left = self.isSubtree(root.left, subRoot)
+        right = self.isSubtree(root.right, subRoot)
+
+        return left or right
